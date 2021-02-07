6d9a9c7c2a1f8f032999e4b1693805a39fec63f6
Hamid Delshadi <hamid.delshadi@gmail.com>
2021-02-07 19:42:03 +0100

    bracket bar chart added

--- script.js
+++ script.js
@@ -1,5 +1,100 @@
 var DataContext = {codes:{}, selectedCountry:"", selectedPlot:"requests", normalizeCountries:false};
+/////////////////////////////////////////////
+function drawGroupBarChart(data, svgId, margin, groupKey, keys){
+  
+  var svgBounds = d3.select(svgId).node().getBoundingClientRect();
 
+  var height = svgBounds.height;
+  var width = svgBounds.width;
+
+  var groups =DataContext.total.map(d=>d[groupKey]);
+    
+  var x0 = d3.scaleBand()
+    .domain(groups)
+    .rangeRound([margin.left, width - margin.right])
+    .paddingInner(0.1)
+
+  var x1 = d3.scaleBand()
+    .domain(keys)
+    .rangeRound([0, x0.bandwidth()])
+    .padding(0.05)
+
+  var y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d3.max(keys, key => d[key]))]).nice()
+      .rangeRound([height - margin.bottom, margin.top])
+
+  color = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+  bar_styler = (bar)  => {
+    return bar.attr("x", d => x1(d.key))
+      .attr("y", d => y(d.value))
+      .attr("width", x1.bandwidth())
+      .attr("height", d => y(0) - y(d.value))
+      .attr("fill", d => color(d.key));
+  }
+
+  const t = d3.transition().duration(500);
+
+  var svg = d3.select(svgId);
+  svg.selectAll("g").data(data).join("g")
+    .attr("transform", d => `translate(${x0(d[groupKey])},0)`)
+    .selectAll("rect")
+    .data(d=>keys.map(key => ({key, value: d[key]})))
+    .join(
+      enter => bar_styler(enter.append("rect")),
+      update => update.call(u => bar_styler(u.transition(t))),
+      exit => exit.remove()
+    );
+  
+  xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)    
+    .call(d3.axisBottom(x0).tickSizeOuter(0))
+
+  yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y).ticks(null, "s"))
+    .call(g => g.select(".tick:last-of-type text").clone()
+        .attr("x", 3)
+        .attr("text-anchor", "start")
+        .text(data.y));
+    
+  svg.append("g")
+      .call(xAxis).selectAll("text")
+      .attr("dx", "-1em").attr("dy", "-0.2em")
+      .attr("transform", "rotate(-65)")
+      .style("font-size", "1.2em")
+      .attr("text-anchor", "end")
+      
+  svg.append("g")
+      .call(yAxis);
+
+  legend = svg => {
+        const g = svg
+            .attr("transform", `translate(${width},10)`)
+            .attr("text-anchor", "end")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+          .selectAll("g")
+          .data(color.domain().slice().reverse())
+          .join("g")
+            .attr("transform", (d, i) => `translate(0,${10 + i * 20})`);
+      
+        g.append("rect")
+            .attr("x", -19)
+            .attr("width", 19)
+            .attr("height", 19)
+            .attr("fill", color);
+      
+        g.append("text")
+            .attr("x", -24)
+            .attr("y", 9.5)
+            .attr("dy", "0.35em")
+            .text(d => d);
+      }
+  svg.append("g")
+      .call(legend);
+}
+/////////////////////////////////////////
 function drawMap(mapData){
   const zoom = d3.zoom()
       .scaleExtent([1, 8])
@@ -186,9 +281,6 @@ function normalizeCountries()
 {
   DataContext.normalizeCountries = !DataContext.normalizeCountries;
   drawCountryBarChart(DataContext.selectedPlot);
-}
-function drawGroupBarChart(){
-  
 }
 function drawCountryBarChart(plotType="requests"){
   d3.select("#country-chart-nav")
@@ -433,6 +525,14 @@ function drawSunburst(data){
   }
   
 }
+
+/////////////////////////////////////
+function drawBracketBarChart(plotType="request"){
+  var margin = ({top: 50, right: 50, bottom: 80, left: 80})
+  drawGroupBarChart(DataContext.total, "#bracket-bar-chart", margin, "bracket",  ["EU", "IT"] );
+}
+
+/////////////////////////////////////
 function loadData(){
   loadYears = function(csv,dataName){
     csv.forEach(d => {
@@ -485,6 +585,18 @@ function loadData(){
     
     drawCountryBarChart();
     drawSunburst(DataContext.EUHierarchy);
+  })
+  d3.csv("data/bracket.csv").then(csv=>{
+    csv.forEach(d=>{
+      d.IT =+d.IT;
+      d.EU =+d.EU;
+      d.residents =+d.residents;
+      d.Normal_IT = +d.Normal_IT;
+      d.Normal_EU = +d.Normal_EU;
+      d.Normal_residents = +d.Normal_residents;
+    })
+    DataContext.bracket = csv;
+    drawBracketBarChart();
   })
   
   d3.json("data/custom.geo.json").then(function(mapData){
