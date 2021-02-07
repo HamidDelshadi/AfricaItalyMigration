79cd26b50f1762eda2d90b654a9e2c5c651173cd
Hamid Delshadi <hamid.delshadi@gmail.com>
2021-02-07 17:14:55 +0100

    sunburst is added

--- script.js
+++ script.js
@@ -187,6 +187,9 @@ function normalizeCountries()
   DataContext.normalizeCountries = !DataContext.normalizeCountries;
   drawCountryBarChart(DataContext.selectedPlot);
 }
+function drawGroupBarChart(){
+  
+}
 function drawCountryBarChart(plotType="requests"){
   d3.select("#country-chart-nav")
     .selectAll("button")
@@ -310,7 +313,126 @@ function drawCountryBarChart(plotType="requests"){
   svg.append("g")
       .call(legend);
 }
+//sunburst////////////////////////
+function drawSunburst(data){
+  var svgId = "#sunburst";
+  var svgBounds = d3.select(svgId).node().getBoundingClientRect();
+  var width = svgBounds.width;
+  radius = width / 6
+  format = d3.format(",d")
+  color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
 
+  arc = d3.arc()
+    .startAngle(d => d.x0)
+    .endAngle(d => d.x1)
+    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .padRadius(radius * 1.5)
+    .innerRadius(d => d.y0 * radius)
+    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
+
+  partition = data => {
+    const root = d3.hierarchy(data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value);
+    return d3.partition()
+        .size([2 * Math.PI, root.height + 1])
+      (root);
+  }
+
+  const root = partition(data);
+
+  root.each(d => d.current = d);
+
+  const svg = d3.select(svgId)
+      .attr("viewBox", [0, 0, width, width])
+      .style("font", "10px sans-serif");
+
+  const g = svg.append("g")
+      .attr("transform", `translate(${width / 2},${width / 2})`);
+
+  const path = g.append("g")
+    .selectAll("path")
+    .data(root.descendants().slice(1))
+    .join("path")
+      .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+      .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+      .attr("d", d => arc(d.current));
+
+  path.filter(d => d.children)
+      .style("cursor", "pointer")
+      .on("click", clicked);
+
+  path.append("title")
+      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+  const label = g.append("g")
+      .attr("pointer-events", "none")
+      .attr("text-anchor", "middle")
+      .style("user-select", "none")
+    .selectAll("text")
+    .data(root.descendants().slice(1))
+    .join("text")
+      .attr("dy", "0.35em")
+      .attr("fill-opacity", d => +labelVisible(d.current))
+      .attr("transform", d => labelTransform(d.current))
+      .text(d => d.data.name);
+
+  const parent = g.append("circle")
+      .datum(root)
+      .attr("r", radius)
+      .attr("fill", "none")
+      .attr("pointer-events", "all")
+      .on("click", clicked);
+
+  function clicked(event, p) {
+    parent.datum(p.parent || root);
+
+    root.each(d => d.target = {
+      x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+      x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+      y0: Math.max(0, d.y0 - p.depth),
+      y1: Math.max(0, d.y1 - p.depth)
+    });
+
+    const t = g.transition().duration(750);
+
+    // Transition the data on all arcs, even the ones that aren’t visible,
+    // so that if this transition is interrupted, entering arcs will start
+    // the next transition from the desired position.
+    path.transition(t)
+        .tween("data", d => {
+          const i = d3.interpolate(d.current, d.target);
+          return t => d.current = i(t);
+        })
+      .filter(function(d) {
+        return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+      })
+        .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+        .attrTween("d", d => () => arc(d.current));
+
+    label.filter(function(d) {
+        return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+      }).transition(t)
+        .attr("fill-opacity", d => +labelVisible(d.target))
+        .attrTween("transform", d => () => labelTransform(d.current));
+  }
+  
+  function arcVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+  }
+
+  function labelTransform(d) {
+    const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+    const y = (d.y0 + d.y1) / 2 * radius;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+  
+  }
+  
+}
 function loadData(){
   loadYears = function(csv,dataName){
     csv.forEach(d => {
@@ -360,7 +482,9 @@ function loadData(){
     DataContext.EUHierarchy = changeToHierarchy(csv, EUKeys);
     DataContext.ITHierarchy = changeToHierarchy(csv, ITKeys);
 
+    
     drawCountryBarChart();
+    drawSunburst(DataContext.EUHierarchy);
   })
   
   d3.json("data/custom.geo.json").then(function(mapData){
@@ -368,11 +492,11 @@ function loadData(){
     drawMap(mapData);
   });//.catch( err =>console.log(err));
 }
+
 function changeToHierarchy(raw,leafKeys){
   keys = ["bracket","country"]
   data = {name:"brackets", children:[]};
 
-  
   raw.forEach(d=>{
     var node = data.children;
     keys.forEach(key =>{
