93f671fa04a14e838240798c9f9745022ee6d2e8
Hamid Delshadi <hamid.delshadi@gmail.com>
2021-02-09 19:56:33 +0100

    vertical chart for countries are added

--- script.js
+++ script.js
@@ -11,6 +11,103 @@ var DataContext = {
   normalizeYears:false,
 };
 /////////////////////////////////////////////
+function drawVerticalGroupBarChart(data, svgId, margin, groupKey, keys){
+  
+  const {xv, yv, width, height} = d3.select(svgId).node().viewBox.baseVal;
+
+  var groups =data.map(d=>d[groupKey]);
+    
+  var y0 = d3.scaleBand()
+    .domain(groups)
+    .rangeRound([margin.top, height - margin.bottom])
+    .paddingInner(0.1)
+
+  var y1 = d3.scaleBand()
+    .domain(keys)
+    .rangeRound([0, y0.bandwidth()])
+    .padding(0.05)
+
+  var x = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d3.max(keys, key => d[key]))]).nice()
+      .rangeRound([ 0,width - margin.right])
+
+  color = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+  bar_styler = (bar)  => {
+    return bar.attr("y", d => y1(d.key))
+      .attr("x", d => margin.left)
+      .attr("height", y1.bandwidth())
+      .attr("width", d => x(d.value))
+      .attr("fill", d => color(d.key));
+  }
+
+  const t = d3.transition().duration(500);
+
+  var svg = d3.select(svgId);
+  svg.selectAll("g").data(data).join("g")
+    .attr("transform", d => `translate(0,${y0(d[groupKey])})`)
+    .selectAll("rect")
+    .data(d=>keys.map(key => ({key, value: d[key]})))
+    .join(
+      enter => bar_styler(enter.append("rect")),
+      update => update.call(u => bar_styler(u.transition(t))),
+      exit => exit.remove()
+    );
+  
+  yAxis = g => g
+  .attr("transform", `translate(${margin.left},0)`)
+    
+    .call(d3.axisLeft(y0).tickSizeOuter(0))
+
+
+  xAxis = g => g
+    .attr("transform", `translate(${margin.left}, ${height - margin.bottom})`)    
+    .call(d3.axisBottom(x).ticks(null, "s"))
+    .call(g => g.select(".tick:last-of-type text").clone()
+    .attr("x", 3)
+    .attr("text-anchor", "start")
+    .text(data.x));
+    
+    
+    
+  svg.append("g")
+      .call(xAxis).selectAll("text")
+      .attr("dx", "-1em").attr("dy", "-0.2em")
+      .attr("transform", "rotate(-65)")
+      .style("font-size", "1.2em")
+      .attr("text-anchor", "end")
+  
+  svg.append("g")
+      .call(yAxis);
+
+  legend = svg => {
+        const g = svg
+            .attr("transform", `translate(${width},10)`)
+            .attr("text-anchor", "end")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+          .selectAll("g")
+          .data(color.domain().slice().reverse())
+          .join("g")
+            .attr("transform", (d, i) => `translate(0,${10 + i * 20})`);
+      
+        g.append("rect")
+            .attr("x", -19)
+            .attr("width", 19)
+            .attr("height", 19)
+            .attr("fill", color);
+      
+        g.append("text")
+            .attr("x", -24)
+            .attr("y", 9.5)
+            .attr("dy", "0.35em")
+            .text(d => d);
+      }
+  svg.append("g")
+      .call(legend);
+}
+
+/////////////////////////////////////////////
 function drawGroupBarChart(data, svgId, margin, groupKey, keys){
   
   const {xv, yv, width, height} = d3.select(svgId).node().viewBox.baseVal;
@@ -342,6 +439,7 @@ function drawCountryBarChart(plotType="requests"){
   DataContext.selectedCountryPlot=plotType;
 
   var margin = ({top: 50, right: 50, bottom: 200, left: 80})
+  var vmargin = ({left: 180, bottom: 50, right:150 , top: 80})
 
   if(plotType=="requests")
     var keys = ["EU", "IT"];
@@ -356,7 +454,9 @@ function drawCountryBarChart(plotType="requests"){
   }
   else
     d3.select("#normalize-country-switch").attr("disabled", "disabled");
-  drawGroupBarChart(DataContext.ds, "#country-bar-chart", margin, "country", keys)
+  
+  drawGroupBarChart(DataContext.ds, "#country-bar-chart-horizontal", margin, "country", keys)
+  drawVerticalGroupBarChart(DataContext.ds, "#country-bar-chart-vertical", vmargin, "country", keys)
 }
 //sunburst////////////////////////
 function drawSunburst(data){
