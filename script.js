de1ce7dc578286491589d15d4ae67f109c2947ad
Hamid Delshadi <hamid.delshadi@gmail.com>
2021-02-17 20:49:01 +0100

    colors changed

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ function drawGroupBarChart(data, svgId, margin, groupKey, keys){
     //.range([ "#ff8080", "#ffcccc"])
     //.range(["#6baed6","#00a140"])
     //.range(["LightSlateGray","#81ccbb"])
-    .range(["#bec4f8","#81ccbb"])
+    .range(["#556089", "#abb2ef"])
     //.range(["#2e83be", "#6bb0d7"])
     
   bar_styler = (bar)  => {
